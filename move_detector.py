df86654071ca954f02de46c9f911356c200dbae0
pawlo555 <spawel555@outlook.com>
2021-03-22 20:58:28 +0100

    Deleting non_max_suppression() because don't have affect to the algorithm

--- move_detector.py
+++ move_detector.py
@@ -6,13 +6,12 @@ import time
 
 class MoveDetector:
     def __init__(self, capture_link=0, box_min_area=800, blur_size=15, min_threshold=10,
-                 supression_threshold=0.5, frames_to_work=10, average_alfa=0.1):
+                 frames_to_work=10, average_alfa=0.1):
         """
         :param capture_link: link to the stream (0 is for local camera)
         :param box_min_area: minimal box area
         :param blur_size: size of the blur, the bigger the larger boxes will be detected
         :param min_threshold:
-        :param supression_threshold: value for non_max_suppression, between 0 and 1.
         :param frames_to_work: frames to start detecting move
         :param average_alfa: change of the background speed, between 0 and 1.
         """
@@ -21,7 +20,6 @@ class MoveDetector:
         self.box_min_area = box_min_area
         self.blur_shape = (blur_size, blur_size)
         self.min_threshold = min_threshold
-        self.supression_threshold = supression_threshold
 
     def process_frame(self, frame):
         """
@@ -56,32 +54,17 @@ class MoveDetector:
 
     def apply_boxes(self, frame, boxes):
         """
-        Adding boxes to the frame, checking box area and do non_max_supression()
+        Adding boxes to the frame, checking box area.
         :param frame: frame to which boxes will be added
         :param boxes: boxes to be added to frame
         :return: frame with boxes
         """
         if boxes is not None:
-            indices = self.apply_non_max_suppression(boxes)
             for i, box in enumerate(boxes):
-                if area(box) > self.box_min_area and i in indices:  # show only big bix
+                if area(box) > self.box_min_area:  # show only big bix
                     x, y, w, h = cv2.boundingRect(box)
                     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         return frame
-
-    def apply_non_max_suppression(self, boxes):
-        """
-        Doing non_max_suppression using cv2
-        :param boxes: array of boxes
-        :return: array of indexes to boxes which non overlap with others
-        """
-        centered_box = []
-        for box in boxes:
-            box_as_array = list(cv2.boundingRect(box))
-            centered_box.append(box_as_array)
-        scores = [1.0 for _ in range(len(boxes))]
-        indices = cv2.dnn.NMSBoxes(centered_box, scores, 0.5, self.supression_threshold)
-        return indices
 
     def loop(self):
         while True:
