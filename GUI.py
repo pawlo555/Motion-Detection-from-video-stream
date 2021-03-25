d6511e4b0687b538f00651201b9b371c77830b43
pawlo555 <spawel555@outlook.com>
2021-03-25 22:43:51 +0100

    gui refactor

--- GUI.py
+++ GUI.py
@@ -4,14 +4,11 @@ from kivy.uix.image import Image
 from kivy.clock import Clock
 from kivy.uix.button import Button
 from kivy.graphics.texture import Texture
-from kivy.core.window import Window
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 import move_detector as md
-
 import cv2
-import numpy
 
 
 class KivyCamera(Image):
@@ -33,49 +30,39 @@ class KivyCamera(Image):
             # display image from the texture
             self.texture = image_texture
 
-
-    # todo
-    def newSource(self, command):
+    def new_source(self, command):
         self.detector = md.MoveDetector(command)
 
-    def newMinBox(self, command):
+    def new_min_box(self, command):
         self.detector.set_box_min_area(command)
 
-    def newBlureSize(self, command):
+    def new_blur_size(self, command):
         self.detector.set_blur_size(command)
 
-    def newMinTreshold(self, command):
+    def new_min_threshold(self, command):
         self.detector.set_min_threshold(command)
 
-    def newAverageAlfa(self, command):
+    def new_average_alfa(self, command):
         self.detector.set_average_alfa(command)
 
-    def newMask(self, command):
+    def new_mask(self, command):
         self.detector.add_mask(command)
 
-    def removeMask(self):
+    def remove_mask(self):
         self.detector.add_mask("no_mask.png")
-        print("mask removed")
 
-    # todo
     def debug1(self):
         # do debug
         self.detector.set_state(md.States.Normal)
 
-    # todo
     def debug2(self):
-        # do debug
         self.detector.set_state(md.States.Background)
 
-    # todo
     def debug3(self):
-        # do debug
         self.detector.set_state(md.States.Threshold)
 
-    # todo
-    def backToStr(self):
-        # kamera wraca do stanu początkowego
-        self.detector.backToDefault()
+    def back_to_str(self):
+        self.detector.back_to_default()
         print("app clean")
 
 
@@ -93,23 +80,16 @@ class MyGrid(GridLayout):
         self.buttonsPanel.rows = 3
         self.txtPanel.cols = 2
 
-        ### inside app
-
+        # inside app
         self.add_widget(self.controlPanel)
         self.add_widget(self.buttonsPanel)
 
-        ###
-
-        ### inside control panel
-
-
+        # inside control panel
         self.my_camera1 = KivyCamera( fps=30)
         self.controlPanel.add_widget(self.my_camera1)
         self.controlPanel.add_widget(self.txtPanel)
 
-        ###
-
-        ### inside buttons panel
+        # inside buttons panel
         self.Button1 = Button(text="Set \nMin Box \nArea \nSubmit")
         self.Button1.bind(on_press=self.pressed1)
 
@@ -145,7 +125,6 @@ class MyGrid(GridLayout):
 
         self.Button12 = Button(text="Exit")
         self.Button12.bind(on_press=self.pressed12)
-        ###
 
         self.buttonsPanel.add_widget(self.Button1)
         self.buttonsPanel.add_widget(self.Button2)
@@ -160,13 +139,12 @@ class MyGrid(GridLayout):
         self.buttonsPanel.add_widget(self.Button11)
         self.buttonsPanel.add_widget(self.Button12)
 
-        ### inside txt panel
+        # inside txt panel
         self.inputPanel = TextInput(multiline=False)
         self.txtPanel.add_widget(self.inputPanel)
 
         self.outputPanel = Label(text="it will be output")
         self.txtPanel.add_widget(self.outputPanel)
-        ###
 
     def pressed1(self, instance):
         # newMinBox
@@ -176,100 +154,91 @@ class MyGrid(GridLayout):
         if command != '':
             a = int(command)
             if a > 0:
-                self.my_camera1.newMinBox(a)
+                self.my_camera1.new_min_box(a)
             else:
                 self.outputPanel.text = "bad value = " + command
         else:
             self.outputPanel.text = "no value given" + command
 
     def pressed2(self, instance):
-        # newMinTreshold DONE
+        # newMinThreshold DONE
         command = self.inputPanel.text
         self.inputPanel.text = ""
-        self.outputPanel.text = "min treshold = \n" + command
+        self.outputPanel.text = "min threshold = \n" + command
         if command != '':
             a = int(command)
             if a > 0:
-                self.my_camera1.newMinTreshold(int(command))
+                self.my_camera1.new_min_threshold(int(command))
             else:
                 self.outputPanel.text = "bad value = " + command
         else:
             self.outputPanel.text = "no value given" + command
 
     def pressed3(self, instance):
-        # Debug1 JUTRO
         self.outputPanel.text = "debug 1 ON"
         self.my_camera1.debug1()
 
     def pressed4(self, instance):
-        # newSoure JUTRO
         command = self.inputPanel.text
         self.inputPanel.text = ""
         self.outputPanel.text = "video path/link/cam = \n" + command
-        if (command == '0'):
+        if command == '0':
             command = 0
         print("Comand:" + str(type(command)))
-        self.my_camera1.newSource(command)
+        self.my_camera1.new_source(command)
 
     def pressed5(self, instance):
-        # newAverageAlfa DONE
         command = self.inputPanel.text
         self.inputPanel.text = ""
         self.outputPanel.text = "average alfa = \n" + command
         if command != '':
             a = float(command)
-            if a >= 0 and a <= 1:
-                self.my_camera1.newAverageAlfa(float(command))
+            if 0 <= a <= 1:
+                self.my_camera1.new_average_alfa(float(command))
             else:
                 self.outputPanel.text = "bad value = " + command
         else:
             self.outputPanel.text = "no value given" + command
 
     def pressed6(self, instance):
-        # SetBlureSize DONE
         command = self.inputPanel.text
         self.inputPanel.text = ""
         self.outputPanel.text = "blure size = \n" + command
         if command != '':
             a = int(command)
-            if (a % 2 == 1 and a > 0):
-                self.my_camera1.newBlureSize(int(command))
+            if a % 2 == 1 and a > 0:
+                self.my_camera1.new_blur_size(int(command))
             else:
                 self.outputPanel.text = "bad value = " + command
         else:
             self.outputPanel.text = "no value given" + command
 
     def pressed7(self, instance):
-        # debug2
         self.outputPanel.text = "debug 2 ON"
         self.my_camera1.debug2()
 
-    # ???
     def pressed8(self, instance):
-        # clean app
         self.outputPanel.text = "app cleaned"
-        self.my_camera1.backToStr()
+        self.my_camera1.back_to_str()
 
-    # DONE
     def pressed9(self, instance):
         # AddMask
         command = self.inputPanel.text
         self.inputPanel.text = ""
         self.outputPanel.text = "mask added, path = \n" + command
-        self.my_camera1.newMask(command)
+        self.my_camera1.new_mask(command)
 
-    def pressed10(self, instance):
+    def pressed10(self):
         # RemoveMask
         self.outputPanel.text = "mask removed"
-        self.my_camera1.removeMask()
+        self.my_camera1.remove_mask()
 
-    def pressed11(self, instance):
+    def pressed11(self):
         # Debug 3
         self.outputPanel.text = "debug 3 ON"
-        print("XD")
         self.my_camera1.debug3()
 
-    def pressed12(self, instance):
+    def pressed12(self):
         # close app
         self.my_camera1.detector.captureStream.release()
         App.get_running_app().stop()
@@ -286,10 +255,3 @@ class MyApp(App):
 
 if __name__ == '__main__':
     MyApp().run()
-
-"""
-DO ZROBIENIA:
-1. 3 debugi 
-2. wczytanie source
-3. podpięcie masek wywala błąd w mask.py (zła wersja pythona???)
-"""
